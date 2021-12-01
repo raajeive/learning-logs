b3b980d3fdf70c45f99074b94b886121a9c6712f
Raajeive K <54172775+raajeive@users.noreply.github.com>
2021-12-01 23:41:27 +0530

    Solved #3
    Updated solution with less time complexity.

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -24,3 +24,20 @@ class Solution(object):
                 if is_palindrome(left, right):
                     longest = max(longest, (left, right), key=lambda x:x[1] - x[0])
         return s[longest[0]:longest[1] + 1]
+
+ 
+# solution 2 O(n^2)
+class Solution(object):
+    def longestPalindrome(self, s):
+        def palindrome(l, r):
+            if s[l] != s[r]:
+                return 0, 0
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r - 1
+
+        longest = (0, 0)
+        for idx in range(len(s) - 1):
+            longest = max(longest, max(palindrome(idx, idx), palindrome(idx, idx + 1), key=lambda x:x[1] - x[0]), key=lambda x:x[1] - x[0])
+        return s[longest[0]:longest[1] + 1]
