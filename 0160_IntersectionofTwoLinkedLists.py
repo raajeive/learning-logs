bce6cb3942fb8b98b2a005cdf1f22eb8ddaf387d
raajeive <54172775+raajeive@users.noreply.github.com>
2022-05-30 23:39:40 +0530

    Added Solution

--- 0160_IntersectionofTwoLinkedLists.py
+++ 0160_IntersectionofTwoLinkedLists.py
@@ -4,7 +4,7 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    def getIntersectionNode(self, headA, headB):
         lengthA = lengthB = 0
 
         tempA = headA
