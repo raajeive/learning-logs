ab477c3db10c452c7ccb07fdd2b92614b9f81283
K Raajeive <k.raajeive@oracle.com>
2025-05-08 09:08:43 +0530

    blind 75 day 3

---
+++ blind75/06_container_with_most.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        if len(height) <= 1:
+            return 0
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+        while left < right:
+            max_water = max(max_water, (right - left) * min(height[left], height[right]))
+            if height[right] > height[left]:
+                left = left + 1
+            else:
+                right = right - 1
+        
+        return max_water
---
+++ blind75/06_word_break.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [True] + [False] * len(s)
+
+        for idx in range(1, len(s) + 1):
+            for word in wordDict:
+                start = idx - len(word)
+                if start >= 0 and dp[start] and s[start:idx] == word:
+                    dp[idx] = True
+
+        return dp[-1]
